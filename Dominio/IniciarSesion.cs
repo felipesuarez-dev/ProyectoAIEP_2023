69be5a5006f76b1cce64b91d63d18508830b2b9f
Felipe Suárez <felipe.suarez@adasme.org>
2023-11-06 11:07:35 -0300

    modificar consulta de método validación en iniciar sesión

--- Dominio/IniciarSesion.cs
+++ Dominio/IniciarSesion.cs
@@ -23,170 +23,6 @@ namespace Dominio
             public bool IsBlocked { get; set; }
         }
 
-        //public bool Validacion(string username, string password)
-        //{
-        //    try
-        //    {
-        //        // Consulta para verificar las credenciales y el estado del usuario
-        //        // Consulta para actualizar los datos del usuario
-        //        string selectQuery = "SELECT Password, IntentosFallidos, IdEstado FROM Usuarios WHERE Username = @username AND passUser = @password";
-        //        string updateQuery = "UPDATE Usuarios SET IntentosFallidos = @IntentosFallidos, IdEstado = @IdEstado WHERE Username = @Username";
-
-        //        using (SqlCommand comando = new SqlCommand(selectQuery, connection))
-        //        {
-        //            comando.Parameters.AddWithValue("@username", username);
-        //            comando.Parameters.AddWithValue("@password", password);
-        //            connection.Open();
-        //            SqlDataReader reader = comando.ExecuteReader(); //guardo el resultado de la consulta en un reader
-
-        //            if (reader.Read())
-        //            {
-        //                //tomo los valores guardados en el reader y transformo sus tipos a string e int, además 
-        //                string storedPassword = reader["Password"].ToString();
-        //                int intentosFallidos = Convert.ToInt32(reader["IntentosFallidos"]);
-        //                int idEstado = Convert.ToInt32(reader["IdEstado"]);
-
-        //                if (idEstado == 2) // 2 es el estado para "Bloqueado"
-        //                {
-        //                    connection.Close();
-        //                    return false; // Usuario bloqueado
-        //                }
-
-        //                if (password == storedPassword)
-        //                {
-        //                    // Restablecer los intentos fallidos en 0, para no bloquear la cuenta posteriormente
-        //                    intentosFallidos = 0;
-        //                    using (SqlCommand actualizarComando = new SqlCommand(updateQuery, connection))
-        //                    {
-        //                        actualizarComando.Parameters.AddWithValue("@IntentosFallidos", intentosFallidos);
-        //                        actualizarComando.Parameters.AddWithValue("@Username", username);
-        //                        actualizarComando.ExecuteNonQuery();
-        //                    }
-        //                    return true;
-        //                }
-        //                else
-        //                {
-        //                    intentosFallidos++;
-
-        //                    // Verificar si se superó el límite de intentos fallidos
-        //                    if (intentosFallidos >= 3)
-        //                    {
-        //                        // Bloquear la cuenta
-        //                        idEstado = 2; // Cambia el estado a "Bloqueado"
-        //                    }
-
-        //                    // Actualizar los intentos fallidos e IdEstado en la base de datos
-        //                    using (SqlCommand actualizarComando = new SqlCommand(updateQuery, connection))
-        //                    {
-        //                        actualizarComando.Parameters.AddWithValue("@IntentosFallidos", intentosFallidos);
-        //                        actualizarComando.Parameters.AddWithValue("@IdEstado", idEstado);
-        //                        actualizarComando.Parameters.AddWithValue("@Username", username);
-        //                        actualizarComando.ExecuteNonQuery();
-        //                    }
-        //                    return false;
-        //                }
-        //            }
-
-        //            connection.Close();
-        //        }
-
-        //        return false; // Autenticación fallida
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new Exception("Error al validar la sesión", ex);
-        //    }
-        //}
-
-        //public ValidationResult Validacion(string username, string password)
-        //{
-        //    ValidationResult result = new ValidationResult();
-
-        //    try
-        //    {
-        //        // Consulta para verificar las credenciales y el estado del usuario
-        //        // Consulta para actualizar los datos del usuario
-        //        string selectQuery = "SELECT pass_user, intentos_fallidos, id_estado FROM Usuarios WHERE username = @username AND pass_user = @pass_user";
-        //        string updateQuery = "UPDATE usuarios SET intentos_fallidos = @intentos_fallidos, id_estado = @id_estado WHERE username = @username";
-
-        //        using (SqlCommand comando = new SqlCommand(selectQuery, connection))
-        //        {
-        //            comando.Parameters.AddWithValue("@username", username);
-        //            comando.Parameters.AddWithValue("@pass_user", password);
-        //            connection.Open();
-        //            SqlDataReader reader = comando.ExecuteReader(); //guardo el resultado de la consulta en un reader
-        //            connection.Close();
-
-        //            if (reader.Read())
-        //            {
-        //                //tomo los valores guardados en el reader y transformo sus tipos a string e int, además 
-        //                string storedUser= reader["username"].ToString();
-        //                string storedPassword = reader["pass_user"].ToString();
-        //                int intentosFallidos = Convert.ToInt32(reader["intentos_fallidos"]);
-        //                int idEstado = Convert.ToInt32(reader["id_estado"]);
-        //                //bool isActive = reader.GetBoolean(reader.GetOrdinal("bol_activo"));
-
-        //                result.IsSuccessful = false;
-        //                result.IsBlocked = false;
-
-        //                if (idEstado == 2) // 2 es el estado para "Bloqueado"
-        //                {
-        //                    result.IsBlocked = true; // Usuario bloqueado
-        //                    return result;
-        //                }
-        //                else if (username != storedUser)
-        //                {
-        //                    result.IsSuccessful = false;
-        //                }
-        //                else if (password == storedPassword)
-        //                {
-        //                    // Restablecer los intentos fallidos en 0, para no bloquear la cuenta posteriormente
-        //                    intentosFallidos = 0;
-        //                    using (SqlCommand actualizarComando = new SqlCommand(updateQuery, connection))
-        //                    {
-        //                        actualizarComando.Parameters.AddWithValue("@intentos_fallidos", intentosFallidos);
-        //                        actualizarComando.Parameters.AddWithValue("@username", username);
-        //                        actualizarComando.ExecuteNonQuery();
-        //                    }
-        //                    result.IsSuccessful = true;
-        //                }
-        //                else
-        //                {
-        //                    intentosFallidos++;
-
-        //                    // Verificar si se superó el límite de intentos fallidos
-        //                    if (intentosFallidos >= 3)
-        //                    {
-        //                        // Bloquear la cuenta
-        //                        idEstado = 2; // Cambia el estado a "Bloqueado"
-        //                    }
-
-        //                    // Actualizar los intentos fallidos e IdEstado en la base de datos
-        //                    using (SqlCommand actualizarComando = new SqlCommand(updateQuery, connection))
-        //                    {
-        //                        actualizarComando.Parameters.AddWithValue("@intentos_fallidos", intentosFallidos);
-        //                        actualizarComando.Parameters.AddWithValue("@id_estado", idEstado);
-        //                        actualizarComando.Parameters.AddWithValue("@username", username);
-        //                        actualizarComando.ExecuteNonQuery();
-        //                    }
-
-        //                    result.IsBlocked = true;
-        //                }
-
-        //                return result; // Devuelve el resultado
-        //            }
-
-        //            connection.Close();
-        //        }
-
-        //        return result; // Autenticación fallida
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new Exception("Error al validar la sesión", ex);
-        //    }
-        //}
-
         public ValidationResult Validacion(string username, string password)
         {
             ValidationResult result = new ValidationResult();
@@ -195,8 +31,8 @@ namespace Dominio
             {
                 // Consulta para verificar las credenciales y el estado del usuario
                 // Consulta para actualizar los datos del usuario
-                string selectQuery = "SELECT pass_user, intentos_fallidos, id_estado FROM Usuarios WHERE username = @username";
-                string updateQuery = "UPDATE usuarios SET intentos_fallidos = @intentos_fallidos, id_estado = @id_estado WHERE username = @username";
+                string selectQuery = "SELECT pass_user, intentos_fallidos, id_estado, bol_activo FROM Usuarios WHERE username = @username";
+                string updateQuery = "UPDATE usuarios SET intentos_fallidos = @intentos_fallidos, id_estado = @id_estado, bol_activo = @bol_activo WHERE username = @username";
 
                 using (SqlCommand comando = new SqlCommand(selectQuery, connection))
                 {
@@ -214,6 +50,7 @@ namespace Dominio
                         string storedPassword = reader["pass_user"].ToString();
                         int intentosFallidos = Convert.ToInt32(reader["intentos_fallidos"]);
                         int idEstado = Convert.ToInt32(reader["id_estado"]);
+                        bool isActive = reader.GetBoolean(reader.GetOrdinal("bol_activo"));
 
                         result.IsSuccessful = false;
                         result.IsBlocked = false;
@@ -260,6 +97,7 @@ namespace Dominio
                             {
                                 // Bloquear la cuenta
                                 result.IsBlocked = true;
+                                isActive = false;
                                 idEstado = 2; // Cambia el estado a "Bloqueado"
                             }
 
@@ -273,7 +111,7 @@ namespace Dominio
                                 actualizarComando.Parameters.AddWithValue("@intentos_fallidos", intentosFallidos);
                                 actualizarComando.Parameters.AddWithValue("@id_estado", idEstado);
                                 actualizarComando.Parameters.AddWithValue("@username", username);
-                                actualizarComando.Parameters.AddWithValue("@bol_activo", true);
+                                actualizarComando.Parameters.AddWithValue("@bol_activo", isActive);
                                 actualizarComando.ExecuteNonQuery();
                             }
 
